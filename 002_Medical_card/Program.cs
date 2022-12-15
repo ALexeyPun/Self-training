b6b183795789082992ac6b769bed034174eaa40d
AlexeyPanshin <dispes@mail.ru>
2022-12-15 20:04:38 +0300

    add medical card

---
+++ 002_Medical_card/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите имя: ");
+string nameUser = Console.ReadLine();
+
+Console.Write("Ведите фамилию: ");
+string surnameUser = Console.ReadLine();
+
+Console.Write("Введите возраст: ");
+int age = Int32.Parse(Console.ReadLine());
+
+Console.Write("Введите вес: ");
+int weight = Int32.Parse(Console.ReadLine());
+
+Console.Write("Введите пол <муж> или <жен>: ");
+string genderEnter = Console.ReadLine();
+bool gender = false;
+if(genderEnter == "муж") gender = true;
+
