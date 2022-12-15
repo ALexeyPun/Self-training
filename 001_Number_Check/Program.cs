55ea4484784557d18ba0c9c36c5b058a7faf9350
AlexeyPanshin <dispes@mail.ru>
2022-12-15 07:25:38 +0300

    add number check

---
+++ 001_Number_Check/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+
+int userNumber = Int32.Parse(Console.ReadLine());
+int result = 0;
+
+if (userNumber < 0) Console.WriteLine("Число ниже 0!");
+else if (userNumber > 10) Console.WriteLine("Число выше 10!");
+else
+{
+    result += userNumber * userNumber;
+    Console.Write("Квадрат числа " + userNumber + " равен " + result + "!");
+}
+
+
