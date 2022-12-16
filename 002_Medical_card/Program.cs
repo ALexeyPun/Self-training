ba9fa7ef4117b2c5dbd2f16e41d2ec6952a4dc82
AlexeyPanshin <dispes@mail.ru>
2022-12-16 03:20:39 +0300

    add medical card

--- 002_Medical_card/Program.cs
+++ 002_Medical_card/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Write("Введите имя: ");
+﻿Console.Clear();
+
+Console.Write("Введите имя: ");
 string nameUser = Console.ReadLine();
 
 Console.Write("Ведите фамилию: ");
@@ -10,8 +12,24 @@ int age = Int32.Parse(Console.ReadLine());
 Console.Write("Введите вес: ");
 int weight = Int32.Parse(Console.ReadLine());
 
-Console.Write("Введите пол <муж> или <жен>: ");
-string genderEnter = Console.ReadLine();
-bool gender = false;
-if(genderEnter == "муж") gender = true;
+bool smallAge = age < 30;
+bool averageAge = age > 30 && age < 40;
+bool greatAge = age > 40;
 
+bool littleWeight = weight < 90;
+bool averageWeight =  weight > 90 && weight < 120;
+bool bigWeight = weight > 120;
+
+
+
+if(smallAge & littleWeight)System.Console.WriteLine("С пациентом, " + nameUser + " " + surnameUser + ", всё в порядке!");
+if(smallAge & averageWeight)System.Console.WriteLine("Пациенту , " + nameUser + " " + surnameUser + ", следует сбросить вес!");
+if(smallAge & bigWeight)System.Console.WriteLine("Пациенту, " + nameUser + " " + surnameUser + ", срочно нужно худеть!");
+
+if(averageAge & littleWeight)System.Console.WriteLine("Пациенту, " + nameUser + " " + surnameUser + ", возможно, следует набрать вес!");
+if(averageAge & averageWeight)System.Console.WriteLine("С пациентом , " + nameUser + " " + surnameUser + ", всё хорошо, но за здоровьем надо следить!");
+if(averageAge & bigWeight)System.Console.WriteLine("Пациенту, " + nameUser + " " + surnameUser + ", нужно худеть, иначе, возможны осложнения!");
+
+if(greatAge & littleWeight)System.Console.WriteLine("Пациент, " + nameUser + " " + surnameUser + "! В вашем возросте - это нормально!");
+if(greatAge & averageWeight)System.Console.WriteLine("С пациентом , " + nameUser + " " + surnameUser + ", всё хорошо, но за здоровьем надо следить. Тем более в приклонном возрасте!");
+if(greatAge & bigWeight)System.Console.WriteLine("Пациенту, " + nameUser + " " + surnameUser + ", нужно пить таблетки от похудения, иначе, ноги сломаются!");
